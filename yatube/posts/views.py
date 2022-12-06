16f859b2c51b9ae0353033037acbd57a91007fe6
Сергей Катаев <nimnul010@yandex.ru>
2022-12-06 21:04:48 +0300

    Черновой рабочий

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,26 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Post
+from .models import Group
  
  
 def index(request):
-    template = 'posts/index.html'
-    title = 'Yatube главная страница'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': title,
-        'text': 'Главная страница'
+        'posts': posts,
     }
-    return render(request, template, context)
- 
- 
-def group_list(request):
-    template = 'posts/group_list.html'
-    title = 'Yatube cписок групп'
-    context = {
-        'title': title,
-        'text': 'Здесь будет информация о группах проекта Yatube'
-    }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
  
  
 def group_posts(request, slug):
-    return HttpResponse(f'Пост №: {slug}')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
